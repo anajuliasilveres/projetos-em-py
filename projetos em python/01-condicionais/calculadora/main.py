2d00e42d8482034dbc7510d936a11a02603c8edd
Ana julia <anajuliasilveres1905@gmail.com>
2026-08-25 16:04:11 -0300

    Update main.py

---
+++ projetos em python/01-condicionais/calculadora/main.py
@@ -0,0 +1,24 @@
+print("digite um número: ")
+numero1 = float(input())
+
+print("digite outro número: ")
+numero2 = float(input())
+
+print("Escolha a operação: ")
+operação = input()
+
+if operação == "+":
+    resultado = numero1 + numero2
+    print ("o resultado é ", f"{resultado:2.2f}") 
+
+elif operação == "-":
+    resultado = numero1 - numero2
+    print ("o resultado é ", f"{resultado:2.2f}") 
+
+elif operação == "*":
+    resultado = numero1 * numero2
+    print ("o resultado é ", f"{resultado:2.2f}")
+
+elif operação == "/":
+        resultado = numero1 / numero2
+        print ("o resultado é ", f"{resultado:2.2f}") 
